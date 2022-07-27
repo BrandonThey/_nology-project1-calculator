dccb56dc3732053b83045c3724833c7e54c0cd99
Brandon They <theyb5220@gmail.com>
2022-07-27 15:58:22 -0500

    Added functionality to the calculator in script.js the calculator is now able to add and subtract two numbers

--- script.js
+++ script.js
@@ -1,12 +1,35 @@
-const numberButtons = document.querySelectorAll(".numberButton");
+const calculatorButtons = document.querySelectorAll("button");
 const outputDisplay = document.querySelector("#display");
 
-const handleNumberClick = (event) => {
-    const element = event.target;
-    outputDisplay.innerHTML = element.innerHTML;
+let operationString = "";
+
+const handleOperation = (operation) => {
+    if(operation.includes("+")){
+        const numbersArr = operation.split("+");
+        const sum = Number(numbersArr[0]) + Number(numbersArr[1]);
+        return String(sum);
+    } else if(operation.includes("-")) {
+        const numbersArr = operation.split("-");
+        const sum = Number(numbersArr[0]) - Number(numbersArr[1]);
+        return String(sum);
+    }
+}
+const handleButtonClick = (event) => {
+    const element = event.target.innerHTML;
+    if(element == "AC"){
+        operationString = "";
+        outputDisplay.innerHTML = "0"
+    } else if(element == "="){
+        operationString = handleOperation(operationString);
+        outputDisplay.innerHTML = operationString;
+        
+    } else{
+        operationString += element;
+        outputDisplay.innerHTML = operationString;
+    }
 }
 
-for(let i = 0; i < numberButtons.length; i++){
-    numberButtons[i].addEventListener("click", handleNumberClick)
+for(let i = 0; i < calculatorButtons.length; i++){
+    calculatorButtons[i].addEventListener("click", handleButtonClick)
 }
 
