dc650f8ef6b310064807b6cfd1f0e8bfc4fed5cf
Brandon They <theyb5220@gmail.com>
2022-07-28 14:47:49 -0500

    Added a second for loop for processing operations in order to keep the order of operations intact

--- script.js
+++ script.js
@@ -4,25 +4,57 @@ const outputDisplay = document.querySelector("#display");
 let operationArr = [];
 
 const handleOperation = (operationArr) => {
-    let number1, number2, operand, value = 0;
+    let number1, number2, operand;
+    
+    //first for loop to handle any multiplication, division or modulo to keep 
+    //the order of operations intact
+    for(let i = 0; i < operationArr.length; i++){
+        operand = operationArr[i];
+        if(operand == "*"){
+            //getting the numbers to the sides of the operand
+            number1 = operationArr[i-1];
+            number2 = operationArr[i+1];
+            //putting the result of the operation to the left side of the operand
+            operationArr[i-1] = (Number(number1) * Number(number2));
+            //remove the operand and the right side from array
+            operationArr.splice(i, 2);
+            //moving i back after splicing to not miss any operands
+            i--;
+        } else if(operand == "/"){
+            //getting the numbers to the sides of the operand
+            number1 = operationArr[i-1];
+            number2 = operationArr[i+1];
+            //putting the result of the operation to the left side of the operand
+            operationArr[i-1] = (Number(number1) / Number(number2));
+            //remove the operand and the right side from array
+            operationArr.splice(i, 2);
+            //moving i back after splicing to not miss any operands
+            i--;
+        } else if(operand == "%"){
+            //getting the numbers to the sides of the operand
+            number1 = operationArr[i-1];
+            number2 = operationArr[i+1];
+            //putting the result of the operation to the left side of the operand
+            operationArr[i-1] = (Number(number1) % Number(number2));
+            //remove the operand and the right side from array
+            operationArr.splice(i, 2);
+            //moving i back after splicing to not miss any operands
+            i--;
+        }   
+    }
+
+    //second for loop that handles addition and subtraction
     number1 = operationArr.shift();
     for(let i = 0; i < operationArr.length; i++){
         operand = operationArr.shift();
         number2 = operationArr.shift();
-        console.log(number1 + " " + operand + " " + number2);
-        if(!isNaN(number1) && !isNaN(number2) && operand == "*"){
-            number1 = (Number(number1) * Number(number2));
-        } else if(!isNaN(number1) && !isNaN(number2) && operand == "/"){
-            number1 = (Number(number1) / Number(number2));
-        } else if(!isNaN(number1) && !isNaN(number2) && operand == "%"){
-            number1 = (Number(number1) % Number(number2));
-        } else if(!isNaN(number1) && !isNaN(number2) && operand == "+"){
+        if(!isNaN(number1) && !isNaN(number2) && operand == "+"){
             number1 = Number(number1) + Number(number2);
         } else if(!isNaN(number1) && !isNaN(number2) && operand == "-"){
             number1 = (Number(number1) - Number(number2));
         }
     }
-    return number1;
+    return String(number1);
 }
 
 const handleButtonClick = (event) => {
@@ -38,9 +70,13 @@ const handleButtonClick = (event) => {
         outputDisplay.innerHTML = oldString + " = " + output;
         
     } else if(element == "+/-"){
-        // make it so that the last number in the string is negative
         const negativeNumber = operationArr.pop()*-1;
         operationArr.push(negativeNumber)
+        output = operationArr.join("");
+        outputDisplay.innerHTML = output;
+    }
+    else if(element == "Redo"){
+        operationArr.pop();
         output = operationArr.join("");
         outputDisplay.innerHTML = output;
     }
